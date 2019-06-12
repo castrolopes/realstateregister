b7b36c350b2b03942707dc194b52ff0ce31cb881
Ricardo Augusto de Castro Lopes <50384588+castrolopes@users.noreply.github.com>
2019-06-11 21:52:33 -0300

    Update realstateregister.sol

--- realstateregister.sol
+++ realstateregister.sol
@@ -40,7 +40,15 @@ contract RealStateRegister {
         livro2[paramEndereco] = novoImovel;
     }
     
-    function devolveNomeProprietario(uint matricula) public view returns (string memory) {
-        return livro1[matricula].nomeProprietario;
+    function devolveTudo(uint matricula) public view returns
+        (string memory, address, string memory, string memory, string memory, string memory, uint256)
+    {
+        return (livro1[matricula].nomeProprietario,
+        livro1[matricula].endereco,
+        livro1[matricula].enderecoRua,
+        livro1[matricula].cidade,
+        livro1[matricula].estado,
+        livro1[matricula].caracteristicas,
+        livro1[matricula].metragem);
     }
 }
