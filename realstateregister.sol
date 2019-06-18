e29e75cdde83aae35808d82ad275bb9a2db1a0f6
Ricardo Augusto de Castro Lopes <50384588+castrolopes@users.noreply.github.com>
2019-06-18 20:23:49 -0300

    Update realstateregister.sol

--- realstateregister.sol
+++ realstateregister.sol
@@ -3,52 +3,60 @@ pragma solidity 0.5.9;
 contract RealStateRegister {
     
     struct Imovel {
-        address endereco;
+        string nomeProprietario;
+        string caracteristicas;
         string enderecoRua;
         string cidade;
         string estado;
-        string caracteristicas;
         uint metragem;
-        string nomeProprietario;
         uint valorVenal;
     }
     
     Imovel[] public livro1;
-    mapping(address => Imovel) public livro2;
+    mapping(string => Imovel) public livro2;
+    mapping(string => Imovel) public livro3;
+    mapping(string => Imovel) public livro4;
+    mapping(string => Imovel) public livro5;
+    mapping(string => Imovel) public livro6;
+    mapping(uint => Imovel) public livro7;
+    mapping(uint => Imovel) public livro8;
     
     function registraImovel(
-        address paramEndereco,
+        string memory paramNomeProprietario,
+        string memory paramCaracteristicas,
         string memory paramEnderecoRua,
         string memory paramCidade,
         string memory paramEstado,
-        string memory paramCaracteristicas,
-        string memory paramNomeProprietario,
         uint paramMetragem,
         uint paramValorVenal) public {
         Imovel memory novoImovel = Imovel(
-            paramEndereco,
+            paramNomeProprietario,
+            paramCaracteristicas,
             paramEnderecoRua,
             paramCidade,
             paramEstado,
-            paramCaracteristicas,
             paramMetragem,
-            paramNomeProprietario,
             paramValorVenal);
             
         livro1.push(novoImovel);
-        
-        livro2[paramEndereco] = novoImovel;
+        livro2[paramNomeProprietario] = novoImovel;
+        livro3[paramCaracteristicas] = novoImovel;
+        livro4[paramEnderecoRua] = novoImovel;
+        livro5[paramCidade] = novoImovel;
+        livro6[paramEstado] = novoImovel;
+        livro7[paramMetragem] = novoImovel;
+        livro8[paramValorVenal] = novoImovel;
     }
     
     function devolveTudo(uint matricula) public view returns
-        (string memory, address, string memory, string memory, string memory, string memory, uint256)
+        (string memory, string memory, string memory, string memory, string memory, uint256, uint256)
     {
         return (livro1[matricula].nomeProprietario,
-        livro1[matricula].endereco,
+        livro1[matricula].caracteristicas,
         livro1[matricula].enderecoRua,
         livro1[matricula].cidade,
         livro1[matricula].estado,
-        livro1[matricula].caracteristicas,
-        livro1[matricula].metragem);
+        livro1[matricula].metragem,
+        livro1[matricula].valorVenal);
     }
 }
